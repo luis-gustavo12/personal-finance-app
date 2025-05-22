e5aab22c20cb511bf34b717cd44a0a6f01874449
Luís Silva <luis.gustavo.ods@gmail.com>
2025-05-22 15:13:00 -0300

    Added expenses new columns on DB, and improved html template

--- web/Finance/src/main/java/com/github/Finance/services/AuthenticationService.java
+++ web/Finance/src/main/java/com/github/Finance/services/AuthenticationService.java
@@ -1,6 +1,11 @@
 package com.github.Finance.services;
 
 import com.github.Finance.dtos.forms.LoginForm;
+import com.github.Finance.models.User;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 /**
@@ -10,7 +15,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class AuthenticationService {
     
+    private final UserService userService;
 
+    public AuthenticationService (UserService userService) {
+        this.userService = userService;
+    }
 
     public boolean authenticateUser(LoginForm loginForm) {
 
@@ -18,6 +27,18 @@ public class AuthenticationService {
         return false;
 
     }
+
+    public User getAuthenticatedUser() {
+
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        return userService.getUserByEmail( userDetails.getUsername() );
+
+    }
+
 
 
 
