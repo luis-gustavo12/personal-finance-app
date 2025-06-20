feab147dc18ccbec14ba8894631233f478d7df6f
Luís Silva <luis.gustavo.ods@gmail.com>
2025-06-20 11:53:36 -0300

    Function usage fix + documentation

--- web/Finance/src/main/java/com/github/Finance/provider/currencyexchange/FrankfurterCurrencyProvider.java
+++ web/Finance/src/main/java/com/github/Finance/provider/currencyexchange/FrankfurterCurrencyProvider.java
@@ -15,6 +15,17 @@ public class FrankfurterCurrencyProvider implements CurrencyExchangeProvider {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    /**
+     * Gets the exchange rate for a given date between two currencies.
+     *
+     * @param fromCurrency The currency you have (e.g., "BRL").
+     * @param toCurrency The currency you want (e.g., "USD").
+     * @param date The date for the exchange rate (e.g., LocalDate.of(2024, 1, 1)).
+     * @return The amount of toCurrency equivalent to one unit of fromCurrency on the given date.
+     *
+     * Example: getExchangeRate("BRL", "USD", LocalDate.of(2024, 1, 1))
+     * returns how many USD one BRL was worth on 2024-01-01.
+     */
     @Override
     public Double getExchangeRate(String fromCurrency, String toCurrency, LocalDate date) {
 
@@ -29,14 +40,15 @@ public class FrankfurterCurrencyProvider implements CurrencyExchangeProvider {
         url = String.format(
         "https://api.frankfurter.dev/v1/%s?base=%s&symbols=%s",
             date,
-            toCurrency, fromCurrency
+            fromCurrency, toCurrency
         );
 
         log.info("URL: {}", url);
 
         Map response = restTemplate.getForObject(url, Map.class);
+        log.info("Response: {}", response);
         Map ratesMap = (Map) response.get("rates");
-        Double rate = (Double) ratesMap.get(fromCurrency);
+        Double rate = (Double) ratesMap.get(toCurrency);
         log.info("Returning amount: [{}]", rate);
         return rate;
 
